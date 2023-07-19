0a3b975af949df54a364d81bc214d59ece954f74
Krathi Shetty <krathi.shetty@odessainc.com>
2023-07-19 10:09:13 +0530

    new-pr

--- application/controllers/Newsroom.php
+++ application/controllers/Newsroom.php
@@ -15,8 +15,8 @@ class Newsroom extends CI_Controller {
 
 
 		$res[ '2' ]->image = '/blog/wp-content/uploads/2021/09/Odessa-investor-THL.png';
-		$res[ '0' ]->image = '/blog/wp-content/uploads/2023/06/sgef-feature.png';
-		$res[ '1' ]->image = '/blog/wp-content/uploads/2023/06/training-feature.png';
+		$res[ '0' ]->image = '/blog/wp-content/uploads/2023/06/cover.svg';
+		$res[ '1' ]->image = '/blog/wp-content/uploads/2023/06/sgef-feature.png';
 
 		$data['meta_title']			= 'Get the latest updates about Odessa in the news.';
 		$data['meta_description']	= 'Odessa\'s market-leading asset finance software is at the heart of many notable businesses such as Dell Financial Services, Truist, and PNC Bank.';
