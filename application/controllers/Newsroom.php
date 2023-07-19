840a000061493658549c8dc3e794fd58e60609cf
Krathi Shetty <krathi.shetty@odessainc.com>
2023-07-19 15:29:43 +0530

    slider-image

--- application/controllers/Newsroom.php
+++ application/controllers/Newsroom.php
@@ -15,7 +15,7 @@ class Newsroom extends CI_Controller {
 
 
 		$res[ '2' ]->image = '/blog/wp-content/uploads/2021/09/Odessa-investor-THL.png';
-		$res[ '0' ]->image = '/blog/wp-content/uploads/2023/06/cover.svg';
+		$res[ '0' ]->image = '/blog/wp-content/uploads/2023/06/cover-svg.jpg';
 		$res[ '1' ]->image = '/blog/wp-content/uploads/2023/06/sgef-feature.png';
 
 		$data['meta_title']			= 'Get the latest updates about Odessa in the news.';
