3c575770d1ba0d7996017c6a1f0baf89109cf14c
Krathi Shetty <krathi.shetty@odessainc.com>
2023-09-04 21:05:37 +0530

    speed-optimize

--- application/controllers/Leadership_amp.php
+++ application/controllers/Leadership_amp.php
@@ -15,12 +15,6 @@ class Leadership_amp extends CI_Controller {
 		$data['tc_description']		= 'Our leadership team is comprised of industry leaders and domain experts, bringing decades of cumulative insight and experience to our company and customers.';
 
 		$data['leaders_list']		= $this->get_list();
-		// Route::get('/company', function () {
-		// 	return redirect('/home/company');
-		// 	});
-// 		$url = 'http://www.sitepoint.com/community/t/redirect-302-response/101550';
-//  header('Location: ' . $url, true, 500);
-// header("HTTP/1.1 404 Not Found");
 		$this->load->view( 'layouts/amp_pages/header.amp.html', $data );
 		$this->load->view('pages/amp_pages/leadership.amp.html', $data);
 		$this->load->view( 'layouts/amp_pages/footer.amp.html', $data );
@@ -52,17 +46,7 @@ class Leadership_amp extends CI_Controller {
 					'linkedin'	=> 'https://www.linkedin.com/in/casumitmaheshwari/',
 				)
 			),
-			// array(
-			// 	'name'			=> 'Jay Mehra',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/jay-mehra.jpg',
-			// 	'title'			=> 'Co-Founder + CTO',
-			// 	'description'	=> 'Jay is Odessa’s CTO and co-founder, overseeing the teams that design and develop the Odessa Platform since 2000. He has a penchant for leveraging technologies to build world-class products, focused on helping Odessa’s customers build great digital experiences. Prior to his role as CTO, Jay was the COO of Odessa from 2000-2019, leading the transformation into a multinational organization meeting global consulting standards in delivery management, performance, and quality. Prior to that, Jay worked for PriceWaterhouseCoopers, LLP where he designed and developed Intellectual Asset Management software. Jay also spent time at Ernst and Young, leading the development of the State of Industry report for India’s auto finance industry. He holds a Bachelor of Science in Mathematical Economics from Haverford College.',
-			// 	'experience'	=> '25 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/jay-mehra-b8b254170/',
-			// 		'twitter'	=> 'https://twitter.com/jayarunmehra'
-			// 	)
-			// ),
+			
 			array(
 				'name'			=> 'Jason St. Laurent',
 				'thumbnail'		=> base_url() .'assets/images/leadership/JasonStLaurent_Odessa.png',
@@ -129,90 +113,7 @@ class Leadership_amp extends CI_Controller {
 					'linkedin'	=> 'https://www.linkedin.com/in/alexellwood/',
 				)
 			),
-			// array(
-			// 	'name'			=> 'Leonard Lane',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/leonard-lane.jpg',
-			// 	'title'			=> 'SVP, Product Management',
-			// 	'description'	=> 'Leonard is Odessa’s SVP, Product Management, overseeing the Product organization and developing the functional roadmap of Odessa Core since 2016. He is a veteran of enterprise systems implementation, coming to Odessa with more than 25 years of experience in accounting, finance, and operations. Prior to Odessa, Leonard held roles at Winthrop Resources Corporation and TCF Bank where he was instrumental in the success of executing digital transformation strategy. He holds a Bachelor of Science and Business in Finance and a Master of Business Administration from the University of Minnesota, and has been a CPA since 1991. As a leasing accounting specialist, Leonard previously served on the ELFA Operations and Technology Committee.',
-			// 	'experience'	=> '28 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/leonard-lane-217b9274/',
-			// 	)
-			// ),
 			
-			// array(
-			// 	'name'			=> 'Sumandeep Kaur',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/SumandeepKaur_Odessa.png',
-			// 	'title'			=> 'SVP, Product Management',
-			// 	'description'	=> 'Joining Odessa as SVP, Product Management in 2020, Sumandeep brings 30+ years of experience in product development and technology, and leading enterprise digital transformation. She is responsible for the strategy and roadmap for the tools and ecosystem of offerings on the Odessa Platform. Prior to Odessa, Sumandeep held varying positions with HP/HPE Financial Services most recently as CIO and Technology Leader for Digital Transformation. She holds a Bachelor of Science and a Masters in Computer Application from Thapar University.',
-			// 	'experience'	=> '24 Years in Leasing',
-			// ),
-			
-			// array(
-			// 	'name'			=> 'Ravi Pathak',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/ravi-pathak.jpg',
-			// 	'title'			=> 'SVP, Technology Architecture',
-			// 	'description'	=> 'Ravi is Odessa’s SVP, Solution Architecture, instrumental in driving the technical architecture behind the company’s market-leading platform strategy. He is a leader in building practical solutions for large-scale enterprise applications and using innovative technology to guarantee customer success. Prior to Odessa, Ravi led the architecture of complex products and high profile, large enterprise applications at Tech Mahindra, DigitE, Mastek and JP Morgan. He holds both a Bachelor of Science and a Master of Science in Computer Engineering from the University of Mumbai.',
-			// 	'experience'	=> '13 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/ravip1/',
-			// 	)
-			// ),
-			
-			
-			// array(
-			// 	'name'			=> 'Jeba Singh',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/jeba-singh.jpg',
-			// 	'title'			=> 'SVP, Technology and Engineering',
-			// 	'description'	=> 'Jeba is Odessa’s SVP, Technology and Engineering, responsible for the development, ongoing management and the roadmaps of all Odessa products. He also leads the dedicated Research and Development department that drives all technological innovation at the company. Passionate about solution architecture, Jeba champions innovation that is impactful and scalable. He joined the company in 2003, and prior to that deployed some of the largest portals built for the Indian market at India Internet Media.',
-			// 	'experience'	=> '20 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/jeba-singh-9904a6b8/',
-			// 	)
-			// ),
-			// array(
-			// 	'name'			=> 'Kevin Schroeder',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/kevin-schroeder.jpg',
-			// 	'title'			=> 'SVP, Delivery Design',
-			// 	'description'	=> 'Kevin is Odessa’s SVP, Delivery Design, leading the Delivery organization in the Americas in strategic initiatives and complex, enterprise implementations. He is passionate about process and performance, driving excellence in Odessa’s implementation methodology. Prior to joining Odessa in 2003, Kevin led the development of models for Fortune 500 companies at PriceWaterhouseCoopers to optimize patent-spend and R&D efforts. He has also previously consulted for various national companies and sports franchises in auditing their capital expenses and budgets. He holds a Bachelor of Science in Mathematical Economics from Haverford College.',
-			// 	'experience'	=> '18 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/kevin-schroeder-8a580a/',
-			// 	)
-			// ),
-			
-			
-		
-			// array(
-			// 	'name'			=> 'Vedapuri Ramanathan',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/vedapuri-ramanathan.jpg',
-			// 	'title'			=> 'SVP, Delivery and Services Development',
-			// 	'description'	=> 'Veda is Odessa’s SVP of Delivery and Services Development, responsible for excellence in systems implementation and customer-focused product development. He is driven by engineering and operational excellence as well as building efficient teams that scale. Prior to joining in 2004, Veda gained experience as an engineer with TCG Software Services, a leading services company in India. He holds a Bachelor of Science in Electronics and a Master’s in Computer Applications from Bharathiar University.',
-			// 	'experience'	=> '18 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/vedapuri-ayalur-ramanathan-27606b2/',
-			// 	)
-			// ),
-			// array(
-			// 	'name'			=> 'Bobbie Warner',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/bobbie-warner.jpg',
-			// 	'title'			=> 'SVP, Delivery Management',
-			// 	'description'	=> 'Bobbie is an SVP, Delivery Management at Odessa, and has been leading our portfolio of customers through the full implementation lifecycle since 2015. A seasoned customer advocate, Bobbie is passionate about customer success and doesn’t shy away from complex projects. She holds several leasing and project certifications including CLFP, PMP and CSM, has a Master of Business Administration from the University of Hartford and a Bachelor of Science in communications from St. Norbert College.',
-			// 	'experience'	=> '9 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/bobbiewarner/',
-			// 	)
-			// ),
-			// array(
-			// 	'name'			=> 'Michael Nyman',
-			// 	'thumbnail'		=> base_url() .'assets/images/leadership/michae-inyman.jpg',
-			// 	'title'			=> 'SVP, Delivery Management',
-			// 	'description'	=> 'Michael is an SVP, Delivery Management at Odessa, responsible for large scale global programs at Odessa since 2018. He uses technology to solve business issues and streamline operations and is passionate about capitalizing on business acumen to ensure customer success. He started his career at HP Financial Services, where he worked for ten years in various roles, later spending seven years at CIT overseeing all leasing platforms. He holds a Bachelor of Science in Accounting from Barton College.',
-			// 	'experience'	=> '23 Years in Leasing',
-			// 	'social'		=> array(
-			// 		'linkedin'	=> 'https://www.linkedin.com/in/michael-nyman-59433a21/',
-			// 	)
-			// ),
 			array(
 				'name'			=> 'Scott Frymire',
 				'thumbnail'		=> base_url() .'assets/images/leadership/scott.png',
