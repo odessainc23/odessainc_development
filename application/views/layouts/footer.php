762ea4f1168f6505eb2de6d957203fe3b5157d52
Krathi Shetty <krathi.shetty@odessainc.com>
2023-10-17 11:10:29 +0530

    twitter-log

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -64,9 +64,8 @@
 				<div class="stripfbox">
 					<div class="socailicons_newsletter">
 					<div class="social_icon"> <a href="https://www.facebook.com/OdessaInc/" target="_blank" aria-label="Facebook"><i class="fa fa-facebook" ></i></a> 
-					<a href="https://twitter.com/OdessaTech" target="_blank" aria-label="Twitter">
-					<img data-src="<?= base_url(); ?>assets/images/x-twitter.svg" alt="" class="lazyload"/>
-					</a> 
+					<a href="https://twitter.com/OdessaTech" target="_blank" aria-label="Twitter">                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style="width: 19px;height: 19px;fill: #fff;"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+</a> 
 					<a href="https://www.linkedin.com/company/odessa-inc-/" target="_blank" aria-label="LinkedIn"><i class="fa fa-linkedin" ></i></a> 
 					<a href="https://www.instagram.com/odessainc/" target="_blank" aria-label="Instagram"><i class="fa fa-instagram" ></i></a>
 					 <a href="https://www.youtube.com/odessainc" target="_blank" aria-label="YouTube"><i class="fa fa-youtube" ></i></a> </div>
