6a7d0160b2f2596514a435a444b00582d14c46d4
Krathi Shetty <krathi.shetty@odessainc.com>
2023-10-31 13:16:21 +0530

    banner-add

--- application/controllers/Automotive_amp.php
+++ application/controllers/Automotive_amp.php
@@ -5,12 +5,12 @@ class Automotive_amp extends CI_Controller {
 
 	public function index(){
 		$data['meta_title']			= 'Automotive Finance Software | Odessa';
-		$data['meta_description']	= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
-		$data['meta_keyword']		= 'XaaS, Billing Software, as-a-service, Subscription Management Software, Pay per use, Usage based billing, Everything as a service, Subscription management platform';
+		$data['meta_description']	= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
+		$data['meta_keyword']		= 'auto finance solution, auto leasing software, vehicle leasing software, car leasing software, auto loan software, car loan software, auto finance software, automotive finance software, auto lending software, automated lending software, auto loan management software, auto loan origination software, vehicle lease management software, auto loan servicing software, vehicle finance software, car finance software, automotive leasing software, auto tracking system, fleet management software, vehicle fleet software, fleet tracking system, fleet management system, fleet maintenance software, fleet software';
 		$data['og_title']			= 'Automotive Finance Software | Odessa';
-		$data['og_description']		= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
+		$data['og_description']		= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
 		$data['tc_title']			= 'Automotive Finance Software | Odessa';
-		$data['tc_description']		= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
+		$data['tc_description']		= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
 		$this->load->view('pages/amp_pages/automotive-finance-software.amp.html', $data);
