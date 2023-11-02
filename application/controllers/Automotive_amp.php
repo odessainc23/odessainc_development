5dc833fae8ff68d4ae78fe4125d620047b64d295
Krathi Shetty <krathi.shetty@odessainc.com>
2023-11-02 10:51:58 +0530

    auto-decription-keywords

--- application/controllers/Automotive_amp.php
+++ application/controllers/Automotive_amp.php
@@ -5,12 +5,12 @@ class Automotive_amp extends CI_Controller {
 
 	public function index(){
 		$data['meta_title']			= 'Automotive Finance Software | Odessa';
-		$data['meta_description']	= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
-		$data['meta_keyword']		= 'auto finance solution, auto leasing software, vehicle leasing software, car leasing software, auto loan software, car loan software, auto finance software, automotive finance software, auto lending software, automated lending software, auto loan management software, auto loan origination software, vehicle lease management software, auto loan servicing software, vehicle finance software, car finance software, automotive leasing software, auto tracking system, fleet management software, vehicle fleet software, fleet tracking system, fleet management system, fleet maintenance software, fleet software';
+		$data['meta_description']	= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
+		$data['meta_keyword']		= 'auto finance solution, auto leasing software, vehicle leasing software, car leasing software, auto loan software, auto finance software, automotive finance software, auto lending software, auto loan management software, auto loan origination software, auto loan servicing software, automotive leasing software, automotive leasing software';
 		$data['og_title']			= 'Automotive Finance Software | Odessa';
-		$data['og_description']		= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
+		$data['og_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
 		$data['tc_title']			= 'Automotive Finance Software | Odessa';
-		$data['tc_description']		= 'Discover Odessa Cutting-Edge Auto Lending and Leasing Software. Elevate Your Vehicle Leasing Experience with Our Car Leasing Software and Automotive Finance Solutions.';
+		$data['tc_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
 		$this->load->view('pages/amp_pages/automotive-finance-software.amp.html', $data);
