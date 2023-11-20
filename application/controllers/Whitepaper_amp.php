a3488009a6a51f91b9337d006f654e468fb9e969
Krathi Shetty <krathi.shetty@odessainc.com>
2023-11-20 13:08:25 +0530

    whitepaper-added

--- application/controllers/Whitepaper_amp.php
+++ application/controllers/Whitepaper_amp.php
@@ -4,13 +4,13 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 class Whitepaper_amp extends CI_Controller {
 
 	public function index(){
-		$data['meta_title']			= 'Transform your business with XaaS solutions from Odessa';
-		$data['meta_description']	= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
-		$data['meta_keyword']		= 'XaaS, Billing Software, as-a-service, Subscription Management Software, Pay per use, Usage based billing, Everything as a service, Subscription management platform';
-		$data['og_title']			= 'Transform your business with XaaS solutions from Odessa';
-		$data['og_description']		= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
-		$data['tc_title']			= 'Transform your business with XaaS solutions from Odessa';
-		$data['tc_description']		= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
+		$data['meta_title']			= '';
+		$data['meta_description']	= '';
+		$data['meta_keyword']		= '';
+		$data['og_title']			= '';
+		$data['og_description']		= '';
+		$data['tc_title']			= '';
+		$data['tc_description']		= '';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
 		$this->load->view('pages/amp_pages/whitepaper.html', $data);
