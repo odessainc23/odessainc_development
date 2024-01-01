4979b58a3b4a8a662166da7da88e193c3a1eb070
ankitvasoya62 <ankit.vasoya@tatvasoft.com>
2024-01-01 18:07:17 +0530

    resolve 404 page issue

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -96,7 +96,7 @@
 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-PSHM9KW');
-	
+	</script>
 </head>
 <body>
 	<!-- Google Tag Manager (noscript) -->
