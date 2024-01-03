13d51ccc4864f62f4f20e8ef14627ef6d1ecf6e5
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-01-03 13:05:30 +0530

    whitepaper edits

--- application/views/layouts/navmenu.php
+++ application/views/layouts/navmenu.php
@@ -26,7 +26,7 @@
                         <div class="covid-alert">
                             <div class="alert  alert-dismissible fade in ">
                                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                                <p class="covid-alert__para text-center" style="text-transform: capitalize;font-size: 18px;">
+                                <p class="covid-alert__para text-center" style="font-size: 18px;">
 								New White paper! Top 10 Must Haves For XaaS Success In Asset Finance <a href="<?php echo base_url(); ?>whitepaper/unlocking-xaas-success-signup" class="odc__btn odc__btn--primary odc__btn--md covid-alert__link alert-link" style="text-decoration: none;margin-left: 10px;height: 35px;min-width:100px;font-weight:400;">Get it Now</a> 
                                                                 <!-- <a target="_blank" href="https://www.odessainc.com/blog/odessa-accelerates-innovation-with-neos-unveils-new-ui-for-spring-22-release/" class="covid-alert__link alert-link">Learn more<i class="fa fa-angle-right"></i></a> -->
                                                             </p>
