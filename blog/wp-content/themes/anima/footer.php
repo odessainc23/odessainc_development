9ddc7d4cd8e69a376d76b3dc954223e008564d0d
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-01-16 15:25:20 +0530

    removed-intercom

--- blog/wp-content/themes/anima/footer.php
+++ blog/wp-content/themes/anima/footer.php
@@ -79,21 +79,6 @@
 </div>
 <?php wp_footer(); ?>
 </body>
-<script>
-	window.intercomSettings = {
-	app_id: "b24vf5ox"
-	};
-	</script> 
-<script>
-	(function(){var w=window;var ic=w.Intercom;if(typeof
-	ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var
-	i=function(
-	){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var
-	l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/b24vf5ox';
-	var x=d.getElementsByTagName('script')[0];x
-	.parentNode.insertBefore(s,x);};if(w.attachEvent){w.atta
-	chEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-</script>
 <script src="<?php bloginfo('template_url'); ?>/assets/js/script.js"></script>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4da41fc07fb2a379"></script>
 </html>
