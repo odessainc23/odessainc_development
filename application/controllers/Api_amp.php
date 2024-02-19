0b4f560b9d8dae8979df6762e5a1f30f21dcb366
Krathi Shetty <krathi.shetty@odessainc.com>
2024-02-19 12:53:03 +0530

    thankyou

--- application/controllers/Api_amp.php
+++ application/controllers/Api_amp.php
@@ -13,7 +13,7 @@ class Api_amp extends CI_Controller {
 		$data['tc_description']		= 'Discover Odessa comprehensive XaaS solutions for financial services, offering flexibility and growth through as-a-service, pay-per-use, and subscription management models. Transform your go-to-market strategy and unlock the potential of the sharing economy with Odessa.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
-		$this->load->view('pages/amp_pages/api.amp.html', $data);
+		$this->load->view('pages/amp_pages/future-ready-asset-fianance-api.html', $data);
 		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
 	}
 }
---
+++ application/controllers/Thankyou_api.php
@@ -0,0 +1,24 @@
+<?php
+defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
+
+class Thankyou_api extends CI_Controller {
+	public function index() {
+		$data = array(
+			'page_title' => 'Thank you for your interest in the white paper',
+			'description' => 'Thank you for your interest. The white paper has been sent to your email.',
+			'keywords' => '',
+			'canonical_tag' => '',
+			'og' => '<meta name="revisit-after" content="7 days" />	
+					 <meta name="allow-search" content="yes" />
+					 <title>Thank you for your interest in the white paper</title>
+					 <meta name="description" content="Thank you for your interest. The white paper has been sent to your email." />
+					 <meta property="og:locale" content="en_US" />',
+			'result' => json_decode( $response )
+		);
+
+		$this->load->view('layouts/amp_pages/header.amp.html', $data);
+		$this->load->view('pages/amp_pages/future-ready-asset-fianance-api-thankyou.html', $data);
+		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
+	}
+
+}
