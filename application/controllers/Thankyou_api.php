8576741d7f6206a07d67a029ec5dee0e6e028564
Krathi Shetty <krathi.shetty@odessainc.com>
2024-02-23 17:09:36 +0530

    banner-design-change

--- application/controllers/Thankyou_api.php
+++ application/controllers/Thankyou_api.php
@@ -4,14 +4,14 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 class Thankyou_api extends CI_Controller {
 	public function index() {
 		$data = array(
-			'page_title' => 'Thank you for your interest in the white paper',
-			'description' => 'Thank you for your interest. The white paper has been sent to your email.',
+			'page_title' => 'Thank you for downloading the whitepaper!',
+			'description' => 'Thank you for downloading the whitepaper!',
 			'keywords' => '',
 			'canonical_tag' => '',
 			'og' => '<meta name="revisit-after" content="7 days" />	
 					 <meta name="allow-search" content="yes" />
 					 <title>Thank you for your interest in the white paper</title>
-					 <meta name="description" content="Thank you for your interest. The white paper has been sent to your email." />
+					 <meta name="description" content="Thank you for downloading the whitepaper!" />
 					 <meta property="og:locale" content="en_US" />',
 			'result' => json_decode( $response )
 		);
