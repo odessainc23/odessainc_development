173daabbb7cfa628db9960f34b6125d54f95731c
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-02-29 14:15:40 +0530

    meta-repeat-delete

--- application/controllers/Platform_amp.php
+++ application/controllers/Platform_amp.php
@@ -30,13 +30,18 @@ class Platform_amp extends CI_Controller {
 	}
 
 	public function core() {
-		$data = array(
-			'page_title' => 'Equipment Leasing Software from Odessa',
-			'description' => 'Odessa provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.',
-			'keywords' => 'equipment lease accounting,equipment leasing software,equipment finance solution,equipment finance software,equipment finance Platform,equipment leasing Platform ',
-			'canonical_tag' => '',
-			
-		);
+		$data	= array();
+		$data['meta_title']			= 'Equipment Leasing Software from Odessa';
+		$data['meta_description']	= 'Odessa provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+		$data['meta_keyword']		= 'equipment lease accounting,equipment leasing software,equipment finance solution,equipment finance software,equipment finance Platform,equipment leasing Platform ';
+		$data['og_title']			= 'Equipment Leasing Software from Odessa';
+		$data['og_description']		= 'Odessa provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+		$data['tc_title']			= 'Equipment Leasing Software from Odessa';
+		$data['tc_description']		= 'Odessa provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+	
+		$this->load->view( 'templates/amp_pages/header.amp.html', $data );
+		$this->load->view('pages/amp_pages/core.amp.html');
+		$this->load->view( 'templates/amp_pages/footer.amp.html' );
 		
 	
 		$this->load->view( 'templates/amp_pages/header.amp.html', $data );
