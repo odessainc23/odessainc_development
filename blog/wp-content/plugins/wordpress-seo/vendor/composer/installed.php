e886d62bfa94c31eeb20328ced7609b98a961910
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-04-04 14:08:22 +0530

    yoast-seo-added

---
+++ blog/wp-content/plugins/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '610163743a1ed97ef26ed7c18ccf22d7f7aec174',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '610163743a1ed97ef26ed7c18ccf22d7f7aec174',
+            'dev_requirement' => false,
+        ),
+    ),
+);
