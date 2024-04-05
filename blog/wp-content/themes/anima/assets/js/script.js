56a8773fe70b9d4a23fdcb79cb739dba87231f7f
Krathi Shetty <krathi.shetty@odessainc.com>
2024-04-05 13:40:41 +0530

    our-people-blurp

--- blog/wp-content/themes/anima/assets/js/script.js
+++ blog/wp-content/themes/anima/assets/js/script.js
@@ -1,1 +1,25 @@
-$(function(){$(".dropdown").on("show.bs.dropdown",function(){$(this).find(".dropdown-menu").first().stop(!0,!0).fadeIn()}),$(".dropdown").on("hide.bs.dropdown",function(){$(this).find(".dropdown-menu").first().stop(!0,!0).fadeOut()}),$("#nav-icon1").click(function(){$(".menuviewwrap").show(),$(this).toggleClass("open"),$(".menuviewwrap").css({width:"100%",visibility:"visible"}),"block"==$(".menuviewwrap").css("display")&&$("html").toggleClass("sidePanel")}),$(".navbar-toggle").click(function(){$(this).toggleClass("active"),$("html").toggleClass("navfixed")}),$(".mobile_view_menu ul li a").click(function(){$(".mobile_view_menu ul li a").removeClass("active"),$(this).addClass("active")})});function tabBx(i){"block"==$("#"+i).css("display")||$(".mobileviewmenu").hide(),$("#"+i).show()}
+$(function() {
+    $(".dropdown").on("show.bs.dropdown", function() {
+        $(this).find(".dropdown-menu").first().stop(!0, !0).fadeIn()
+    }), $(".dropdown").on("hide.bs.dropdown", function() {
+        $(this).find(".dropdown-menu").first().stop(!0, !0).fadeOut()
+    }), $("#nav-icon1").click(function() {
+        $(".menuviewwrap").show(), $(this).toggleClass("open"), $(".menuviewwrap").css({
+            width: "100%",
+            visibility: "visible"
+        }), "block" == $(".menuviewwrap").css("display") && $("html").toggleClass("sidePanel")
+    }), $(".navbar-toggle").click(function() {
+        $(this).toggleClass("active"), $("html").toggleClass("navfixed")
+    }), $(".mobile_view_menu ul li a").click(function() {
+        $(".mobile_view_menu ul li a").removeClass("active"), $(this).addClass("active")
+    })
+      // New URL
+  var newUrl = "https://www.odessainc.com/whitepaper/unlocking-xaas-success-signup";
+
+  // Change the href attribute of the anchor tag
+  $("#post-1036 .continue-reading-link").attr("href", newUrl);
+});
+
+function tabBx(i) {
+    "block" == $("#" + i).css("display") || $(".mobileviewmenu").hide(), $("#" + i).show()
+}
