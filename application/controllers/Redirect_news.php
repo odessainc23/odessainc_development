1dac5d628c5d0a9ce4b2c2d5f58ca8f84631336d
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-04-26 01:12:34 +0530

    news-redirect

---
+++ application/controllers/Redirect_news.php
@@ -0,0 +1,20 @@
+<?php
+defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
+
+class Redirect_news extends CI_Controller {
+
+	public function index(){
+		$data['meta_title']			= 'Auto Finance Software | Odessa';
+		$data['meta_description']	= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
+		$data['meta_keyword']		= 'auto finance solution, auto leasing software, vehicle leasing software, car leasing software, auto loan software, auto finance software, automotive finance software, auto lending software, auto loan management software, auto loan origination software, auto loan servicing software, automotive leasing software, automotive leasing software';
+		$data['og_title']			= 'Auto Finance Software | Odessa';
+		$data['og_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
+		$data['tc_title']			= 'Auto Finance Software | Odessa';
+		$data['tc_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
+
+		$this->load->view('layouts/amp_pages/header.amp.html', $data);
+		// $this->load->view('pages/amp_pages/redirection.html', $data);
+		$this->load->view('pages/amp_pages/auto-finance-news.html', $data);
+		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
+	}
+}
