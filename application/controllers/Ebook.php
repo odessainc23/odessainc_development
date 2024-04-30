c1fa2a5e90293357b6bcf2aff0b9238349de030d
Krathi Shetty <krathi.shetty@odessainc.com>
2024-04-30 12:24:41 +0530

    CX-handbook

--- application/controllers/Ebook.php
+++ application/controllers/Ebook.php
@@ -4,16 +4,16 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 class Ebook extends CI_Controller {
 
 	public function index(){
-		$data['meta_title']			= 'Unlocking Asset Finance Futures: Mastering API Strategies for Success';
-		$data['meta_description']	= "Dive into our comprehensive whitepaper, 'Banking on APIs: The Asset Finance Edition', and learn how to harness the power of APIs to build a future-ready asset finance system";
-		$data['meta_keyword']		= 'asset finance software, asset leasing software, asset finance solution, asset finance management software, ai in asset finance, strategic ai adoption, maximizing business impact, challenges in ai implementation, asset finance industry insights';
+		$data['meta_title']			= 'Get the latest handbook Customer Experiences in Asset Finance | Odessa';
+		$data['meta_description']	= "Delve into our latest e-book to uncover how you can WOW your customers  and create standout experiences that build lifelong customer relationships at AFSA";
+		$data['meta_keyword']		= 'asset finance industry, as-a-service, asset finance software, customer experiences in asset finance';
 		$data['og_title']			= 'Unlocking Asset Finance Futures: Mastering API Strategies for Success';
-		$data['og_description']		= "Dive into our comprehensive whitepaper, 'Banking on APIs: The Asset Finance Edition', and learn how to harness the power of APIs to build a future-ready asset finance system";
-		$data['tc_title']			= 'Unlocking Asset Finance Futures: Mastering API Strategies for Success';
-		$data['tc_description']		= "Dive into our comprehensive whitepaper, 'Banking on APIs: The Asset Finance Edition', and learn how to harness the power of APIs to build a future-ready asset finance system";
+		$data['og_description']		= "Delve into our latest e-book to uncover how you can WOW your customers  and create standout experiences that build lifelong customer relationships at AFSA";
+		$data['tc_title']			= 'Get the latest handbook Customer Experiences in Asset Finance | Odessa';
+		$data['tc_description']		= "Delve into our latest e-book to uncover how you can WOW your customers  and create standout experiences that build lifelong customer relationships at AFSA";
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
-		$this->load->view('pages/amp_pages/customer-experience-asset-finance.html', $data);
+		$this->load->view('pages/amp_pages/cx-asset-finance.html', $data);
 		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
 	}
 }
