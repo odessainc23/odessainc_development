7d42da86bde88dcca7bed3bf91ca0d0bd9f6bcf6
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-05-09 12:07:52 +0530

    distributer-banner-add

--- application/controllers/Auto_handbook.php
+++ application/controllers/Auto_handbook.php
@@ -4,12 +4,12 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 class Auto_handbook extends CI_Controller {
 
 	public function index(){
-		$data['meta_title']			= 'The Business Case for Next-Gen Auto Platform';
+		$data['meta_title']			= 'The Business Case for a Next-Gen Auto Finance Platform';
 		$data['meta_description']	= 'Unlock the potential of Odessa\'s auto finance software with our latest handbook. Streamline operations, boost efficiency & drive growth. Download now.';
 		$data['meta_keyword']		= 'auto finance software, auto leasing software, XaaS';
-		$data['og_title']			= 'The Business Case for Next-Gen Auto Platform';
+		$data['og_title']			= 'The Business Case for a Next-Gen Auto Finance Platform';
 		$data['og_description']		= 'Unlock the potential of Odessa\'s auto finance software with our latest handbook. Streamline operations, boost efficiency & drive growth. Download now.';
-		$data['tc_title']			= 'The Business Case for Next-Gen Auto Platform';
+		$data['tc_title']			= 'The Business Case for a Next-Gen Auto Finance Platform';
 		$data['tc_description']		= 'Unlock the potential of Odessa\'s auto finance software with our latest handbook. Streamline operations, boost efficiency & drive growth. Download now.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
