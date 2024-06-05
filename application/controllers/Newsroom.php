8ae3afd85a2bc38c4f0b0ffa1791799dcc6aea91
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-06-05 10:55:19 +0530

    nate

--- application/controllers/Newsroom.php
+++ application/controllers/Newsroom.php
@@ -15,9 +15,9 @@ class Newsroom extends CI_Controller {
 		$res_announcements = $query1->result();
 
 
-		$res[ '2' ]->image = '/blog/wp-content/uploads/2023/Bob.jpg';
-		$res[ '0' ]->image = '/blog/wp-content/uploads/2024/auto-press.png';
-		$res[ '1' ]->image = '/blog/wp-content/uploads/2023/john.jpeg';
+		$res[ '2' ]->image = '/blog/wp-content/uploads/2023/john.jpeg';
+		$res[ '0' ]->image = '/blog/wp-content/uploads/2024/nate.jpg';
+		$res[ '1' ]->image = '/blog/wp-content/uploads/2024/auto-press.png';
 
 
 		$data['meta_title']			= 'Get the latest updates about Odessa in the news.';
