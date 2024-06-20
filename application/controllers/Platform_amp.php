cceb24e21a1610dd49afec72a1f747f9890741d8
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-06-20 12:28:06 +0530

    core-tags-fix

--- application/controllers/Platform_amp.php
+++ application/controllers/Platform_amp.php
@@ -30,21 +30,18 @@ class Platform_amp extends CI_Controller {
 	}
 
 	public function core() {
-		$data = array(
-			'page_title' => 'Equipment Leasing Software from Odessa',
-			'description' => 'Odessa provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.',
-			'keywords' => 'equipment lease accounting,equipment leasing software,equipment finance solution,equipment finance software,equipment finance Platform,equipment leasing Platform ',
-			'canonical_tag' => '',
-			'og' => '<meta name="revisit-after" content="7 days" />	
-	<meta name="allow-search" content="yes" />
-<title>Build on our flexible asset management platform</title><meta name="description" content="Odessa is an asset management system that unifies your origination and servicing, reporting and analytics on one robust platform"/>
-	<meta property="og:locale" content="en_US"/>'
-		);
-		
-	
+		$data	= array();
+		$data['meta_title']			= 'Equipment Leasing Software from Odessa';
+		$data['meta_description']	= 'Odessa\'s provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+		$data['meta_keyword']		= 'equipment lease accounting,equipment leasing software,equipment finance solution,equipment finance software,equipment finance Platform,equipment leasing Platform';
+		$data['og_title']			= 'Equipment Leasing Software from Odessa';
+		$data['og_description']		= 'Odessa\'s provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+		$data['tc_title']			= 'Equipment Leasing Software from Odessa';
+		$data['tc_description']		= 'Odessa\'s provides equipment leasing software to unify your origination and servicing, reporting and analytics - all on one robust platform.';
+
 		$this->load->view( 'templates/amp_pages/header.amp.html', $data );
 		$this->load->view('pages/amp_pages/core.amp.html');
 		$this->load->view( 'templates/amp_pages/footer.amp.html' );
-		
 	}
+
 }
