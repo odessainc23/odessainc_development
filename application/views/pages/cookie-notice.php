6ad6f66d3dd244c3b9168c87a54540f7bb4ad40f
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-06-25 11:29:00 +0530

    cookie-page-update

--- application/views/pages/cookie-notice.php
+++ application/views/pages/cookie-notice.php
@@ -2,21 +2,14 @@
 	.legal-page{
 		margin:120px 0 0
 	}
-  .checklist {
-  font-size: 1.2rem;
-  list-style-type: none;
-  margin: 10px 20px 30px 20px !important;
-  padding: 0;
+  .legal-phtitle {
+    margin-bottom: 10px;
 }
-.legal-phtitle{
-  margin-bottom:10px
-}
-.checkmark, ul.checklist li {
-  position: relative;
-  padding-left:0;
-}
-.legal-phicon{
-  width: 50px;
+  .legal-pcontent p{
+    position:relative
+  }
+  .legal-phicon {
+    width: 50px;
     height: 50px;
     position: relative;
     background: url(../assets/images/cookies.png) 0 0 no-repeat;
@@ -24,8 +17,8 @@
     margin: 0 auto;
     background-size: cover;
 }
-.checkmark::before, ul.checklist li::before {
-  top: 8px;
+  .bullets:before{
+    top: 8px;
     font-size: 20px;
     background: #2a2d36;
     font-weight: 700;
@@ -35,9 +28,13 @@
     position: absolute;
     border-radius: 50%;
     left: -15px;
- 
-}
-
+  }
+  .legal-ppost p{
+    font-size:17px
+  }
+  .legal-ppost h4{
+    margin: 20px 0 10px 0;
+  }
 	</style>
 
 
@@ -49,30 +46,136 @@
 				<span class="legal-phicon"></span>
 			</div>
       <section class="legal-ppost">
-       
+        <div class="legal-ppnav">
+          <div class="legal-ppnav__inner">
+            <div class="legal-ppnavtitle">TABLE OF CONTENTS</div>
+            <div class="legal-ppnavlist">
+              <a href="#Collect" class="active">1. Purpose & Scope</a>
+              <a href="#How">2. What are Cookies?</a>
+              <a href="#Uses">3. Use of Cookies and Similar Technologies</a>
+              <a href="#Correct">4.	How Long do Cookies Stay on My Device?</a>
+			  <a href="#Restrict">5. How does Odessa Provide Online and Mobile Advertising?</a>
+			  <a href="#Share">6.	How do I Manage Cookies?</a>
+			  <a href="#Handle">7. What about Similar Tracking Technologies?</a>
+			  <a href="#Linked">8. How does Odessa respond to privacy-preference signals?</a>
+        <a href="#Privacy">9. Privacy at Odessa</a>
+			  <a href="#Contact">10. How to Contact Us</a>
+
+              <!-- <a href="#SourceCode">Source Code Escrow</a> -->
+
+              <!-- <a href="#QlikTerms">Qlik Terms</a> -->
+            </div>
+          </div>
+        </div>
 		<div class="legal-ppcontent">
         
           <div id="Collect" class="legal-pcontent legal-ppcontab">
 			<!-- <h3></h3> -->
-            <p>A cookie is a small piece of data (text file) that a website – when visited by a user – asks your browser to store on your device in order to remember information about you, such as your language preference or login information. Those cookies are set by us and called first-party cookies. We also use third-party cookies – which are cookies from a domain different than the domain of the website you are visiting – for our advertising and marketing efforts.</p>
-            <p>More specifically, we use cookies and other tracking technologies for the following purposes:</p>
-            <ul class="checklist">
-              <li>Assisting you in navigation;</li>
-              <li>Assisting in registration to our events, login, and your ability to provide feedback;</li>
-              <li>Analyzing your use of our products, services or applications;</li>
-              <li>Assisting with our promotional and marketing efforts. (including behavioral advertising)</li>
-            </ul>
-            <p>Below is a detailed list of the cookies we use on our Website. Our Website is scanned with our cookie scanning tool regularly to maintain a list as accurate as possible. We classify cookies in the following categories:</p>
-            <ul class="checklist">
-              <li>Strictly Necessary Cookies</li>
-              <li>Performance Cookies</li>
-              <li>Functional Cookies</li>
-              <li>Targeting Cookies</li>
-            </ul>
+            <h4>1. Purpose & Scope</h4>
+            <p>Our Sites use Cookies and Similar Technologies (as defined below) for various purposes, including to ensure that we give our visitors the best possible experience by providing you with personalized content, remembering your marketing preferences, and helping you to obtain the right information on our Sites.</p>
+            <!-- <p>This Privacy Policy explains:</p> -->
+             <p>You can find out more about Cookies and Similar Technologies, how we use them as well as your rights to control our use of them below.</p>
+            <p>You can manage your preferences in relation to Cookies and Similar Technologies used on our Sites by using the Manage Cookie Settings tool.</p>
+
+			<section id="How">
+            <h4>2. Changes to this Privacy Policy</h4>
+            <p>Cookies are small files—usually consisting of letters and numbers—placed on your computer, tablet, phone, or similar device, when you use that device to visit a web site. Cookies are widely used by website owners to make their web sites operate, work more efficiently, and provide analytic information.</p>
+            <p>Cookies may be placed on your device by Odessa and third parties. We recommend using the Manage Cookie Setting tool to express your cookie choices.</p>
+			</section>
+
+			<section id="Uses">
 			
+			<h4>3. Use of Cookies and Similar Technologies</h4>
+            <p>This Notice describes how Odessa, our vendors, and other third parties on our behalf use Cookies and Similar Technologies (such as pixel tags, web beacons, clear GIFs, JavaScript, and local storage) ("Cookies") in connection with communications sent by Odessa or on our behalf and the websites (including mobile websites and apps) owned and controlled by Odessa (the “Sites”).</p>
+            <p>The <a href="https://dev.odessainc.com/privacy-policy" target="_blank">Odessa Privacy Notice</a> will apply in addition to this Notice where we collect and use personal information through the Cookies.</p>
+            <p>Listed below are the different categories of Cookies that Odessa and vendors on our behalf may use on our Sites:</p>
+           <div class="inner">
+              <p>
+              <span class="bullets"></span> <span class="">Strictly Necessary Cookies: </span> These technologies are necessary for the Odessa experience to function and cannot be switched off in our systems. The technology is usually only set in response to actions made by the device owner which amount to a request for services, such as setting privacy preferences, logging in, filling in forms, maintaining secure login areas, maintaining state across pages (remembering items in a shopping basket), and server load balancing. The device owner can set their preference to block or alert Odessa about these technologies, but some parts of the Odessa experience will not work. These technologies do not store any personally identifiable information.
+              </p>
+              <p>
+              <span class="bullets"></span> <span class="">Analytics Cookies: </span> These technologies allow Odessa to count device visits and traffic sources, so Odessa can measure and improve the performance of our experiences. The technology helps Odessa to know which experiences are the most and least popular and see how device owners interact with the experience. All information these technologies collect is aggregated. If the device owner does not allow these technologies, then Odessa will not know when the device owner visited or how the device owner interacted with our experiences.
+              </p>
+              <p>
+              <span class="bullets"></span><span class="">Functional Cookies: </span> These technologies enable the Odessa experience to provide enhanced functionality and personalization. The technology may be set by Odessa or by third-party providers whose services Odessa have added to our experiences. If the device owner does not allow these technologies, then some or all of these services may not function properly for the device owner.
+              </p>
+              <p>
+              <span class="bullets"></span><span class="">Ad Targeting Cookies: </span> These technologies may be set through our Odessa experience or by our advertising partners. The technology may be used to build a profile of the device owner’s interests and show the device owner relevant advertisements on other experiences. The technology does not store directly personal information on the device, but the technology is based on uniquely identifying the device. If the device owner does not allow these technologies, then the device owner will experience less targeted advertising.
+              </p>
+              
+            </div>
+            <section>
+
+			<section id="Correct">
+      <h4>4. How Long do Cookies Stay on My Device?</h4>
+              <p>Some Cookies operate from the time you visit a Site until the end of that particular browsing session. These "session" Cookies expire and are automatically deleted when you close your Internet browser.</p>
+              <p>Some Cookies stay on your device between browsing sessions—these "persistent" Cookies do not expire when you close your browser. The length of time a persistent Cookie stays on your device varies from Cookie to Cookie.
+              </p>
+           
+			</section>
+			
+		
+			
+			<section id="Restrict">
+			<h4>5. How does Odessa Provide Online and Mobile Advertising?</h4>
+      <p>We and our vendors, and our other third parties, engage in interest-based advertising to deliver advertisements and personalized content that we and other advertisers believe will be of interest to you. Third party vendors will be responsible for their own use of personal information collected through the Cookies.</p>
+      <p>Advertisements may be delivered to you based on your online or mobile behavior (on Odessa Sites and non-Odessa sites), your search activity, your responses to one of our advertisements or communications, the pages you visit, your general geographic location, or other information. These ads may appear on our Sites or on third party websites. The vendors with whom we work to help us conduct interest-based advertising may be members of self-regulatory associations such as the <a href="https://www.networkadvertising.org/understanding-online-advertising/" target="_blank">Network Advertising Initiative (NAI)</a> and <a href="https://youradchoices.com/" target="_blank">the Digital Advertising Alliance (DAA)</a>. For Sites directed at persons located in the European Union, we may work with vendors who are members of the <a href="https://www.youronlinechoices.com/" target="_blank">European Digital Advertising Alliance (eDAA)</a>. You may also see advertisements for third parties on Odessa Sites or other websites or properties, based on your visits to, and activities on, Odessa Sites and other sites.</p>
+      <p>See "How do I Manage Cookies?" below for information on how to opt out of advertising cookies.</p>
+			</section>
+
+			<section id="Share">
+			<h4>6. How do I Manage Cookies?</h4>
+      <p>You can decide whether or not to accept Cookies. One way you can do this is through your Internet browser’s settings. Most Internet browsers allow some control of Cookies through the browser settings. If you use your browser settings to block all Cookies you may not be able to access parts of our Sites. The following links take you to information on how to manage the Cookies settings on some popular browsers:</p>
+      <div class="inner">
+        <p><span class="bullets"></span> <a href="https://support.apple.com/en-in/guide/safari/sfri11471/mac" target="_blank">Apple Safari</a>
+        </p>
+        <p><span class="bullets"></span> <a href="https://support.google.com/chrome/answer/95647?hl=en&p=cpn_cookies" target="_blank">Google Chrome</a>
+        </p>
+        <p><span class="bullets"></span> <a href="https://support.microsoft.com/en-us/windows/manage-cookies-in-microsoft-edge-view-allow-block-delete-and-use-168dab11-0753-043d-7c16-ede5947fc64d#ie=ie-11" target="_blank">Microsoft Internet Explorer</a> and <a href="https://support.microsoft.com/en-us/help/4027947/microsoft-edge-delete-cookies" target="_blank">Microsoft Edge</a>
+        </p>
+        <p><span class="bullets"></span> <a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer" target="_blank">Mozilla Firefox</a>
+        </p>
+      </div>	
+      <p>You can manage your preferences in relation to Cookies and Similar Technologies served on our Sites using the Manage Cookie Settings tool. You can also use a browser plug-in such as Ghostery from Cliqz International GmbH to turn off Cookies. You can opt out of the use of Cookies to tailor content or advertising to you by visiting the links below. Note that if you choose to opt out, you won’t necessarily see fewer ads. However, the ads you see may be less relevant to your interests.</p>
+      <div class="inner">
+        <p><span class="bullets"></span> <a href="http://www.networkadvertising.org/managing/opt_out.asp" target="_blank">http://www.networkadvertising.org/managing/opt_out.asp</a>
+        </p>
+        <p><span class="bullets"></span> <a href="http://www.aboutads.info/choices" target="_blank">http://www.aboutads.info/choices</a>
+        </p>
+      </div>
+      </section>
+
+			<section id="Handle">
+			<h4>7. What about Similar Tracking Technologies?</h4>
+      <p>Cookies are not the only way to recognize or track visitors to a website. Odessa may use other, similar technologies from time to time, like web beacons (sometimes called "tracking pixels" or "clear gifs"). These are tiny graphics files that contain a unique identifier that enable us to recognize when someone has visited our Sites or opened a communication that we have sent them. This enables us to assess the effectiveness of communications. In many instances, these technologies are reliant on cookies to function properly, and so declining cookies will impair their functioning.</p>
+			</section>
+			
+			<section id="Linked">
+			<h4>8. How does Odessa respond to privacy-preference signals?</h4>
+        <p>If you visit an Odessa Site or service using a device with a United States IP address and a privacy-preference signal enabled (e.g., <a href="https://globalprivacycontrol.org/" target="_blank">Do Not Track, Global Privacy Control</a>), Odessa will not collect device information for the purpose of Ad Targeting uses. If you have more than one device, you will need to configure all devices with your privacy-preferences. For more information, you can visit the Ad Targeting category found in the Manage Cookie Setting tool and review your Ad Targeting and Sharing Choices.</p>
+          </section>
+
+		
+
+			<section id="Privacy">
+            <h4>9. Privacy at Odessa</h4>
+            <p>Odessa is committed to privacy. For more information about Odessa's approach to privacy see our <a href="https://dev.odessainc.com/privacy-policy" target="_blank">Odessa Privacy 
+            </a> </p>
+            
+
+			</section>
+      <section id="Contact">
+            <h4>10.	How to Contact Us</h4>
+            <p>If you have any questions about how Odessa uses Cookies, you can contact us or send a letter to the mailing address below. Please include your contact information, the name or hyperlink of the Odessa Site or service, and a description of your Cookie request.</p>
+            <p>Odessa Technologies, Inc. 50 S 16th Street</p>
+            <p>Suite 1900</p>
+            <p>Philadelphia, PA 19102</p>
+            <p>Attention: Data Protection Officer</p>
+			</section>
             </div>
           </div>
-        
+        </div>
+    
       </section>
     </div>
   </div>
