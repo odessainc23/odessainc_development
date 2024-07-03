9793cd0dd6c1cfcebead8756c1f6c886a0d7c1c5
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-07-03 17:10:01 +0530

    redirect-innovate

--- application/controllers/Redirect_innovate24.php
+++ application/controllers/Redirect_innovate24.php
@@ -4,13 +4,13 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 class Redirect_innovate24 extends CI_Controller {
 
 	public function index(){
-		$data['meta_title']			= 'Auto Finance Software | Odessa';
-		$data['meta_description']	= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
-		$data['meta_keyword']		= 'auto finance solution, auto leasing software, vehicle leasing software, car leasing software, auto loan software, auto finance software, automotive finance software, auto lending software, auto loan management software, auto loan origination software, auto loan servicing software, automotive leasing software, automotive leasing software';
-		$data['og_title']			= 'Auto Finance Software | Odessa';
-		$data['og_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
-		$data['tc_title']			= 'Auto Finance Software | Odessa';
-		$data['tc_description']		= 'Explore Odessa\'s State-of-the-Art Auto Finance Solution and Enhance Your Auto Leasing Experience with Our Innovative Auto Finance Software.';
+		$data['meta_title']			= 'Summary - Odessa Client Conference';
+		$data['meta_description']	= 'Summary - Odessa Client Conference';
+		$data['meta_keyword']		= 'Summary - Odessa Client Conference';
+		$data['og_title']			= 'Summary - Odessa Client Conference';
+		$data['og_description']		= 'Summary - Odessa Client Conference';
+		$data['tc_title']			= 'Summary - Odessa Client Conference';
+		$data['tc_description']		= 'Summary - Odessa Client Conference';
 
 		// $this->load->view('layouts/amp_pages/header.amp.html', $data);
 		// $this->load->view('pages/amp_pages/redirection.html', $data);
