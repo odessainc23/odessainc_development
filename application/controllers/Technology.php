650a461a5d2eaf3a758c537640aa6a4a2ec0f0b7
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-07-29 17:07:14 +0530

    new-landing-page

---
+++ application/controllers/Technology.php
@@ -0,0 +1,24 @@
+<?php
+defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
+
+class Technology extends CI_Controller {
+	public function index() {
+		$data = array(
+			'page_title' => 'Thank you for downloading the whitepaper!',
+			'description' => 'Thank you for downloading the whitepaper!',
+			'keywords' => '',
+			'canonical_tag' => '',
+			'og' => '<meta name="revisit-after" content="7 days" />	
+					 <meta name="allow-search" content="yes" />
+					 <title>Thank you for your interest in the white paper</title>
+					 <meta name="description" content="Thank you for downloading the whitepaper!" />
+					 <meta property="og:locale" content="en_US" />',
+			'result' => json_decode( $response )
+		);
+
+		$this->load->view('layouts/amp_pages/header.amp.html', $data);
+		$this->load->view('pages/amp_pages/technology-partner.html', $data);
+		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
+	}
+
+}
