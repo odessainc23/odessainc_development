7ba4ecc18e092715113d73bfcd07531136ab40bf
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-09-02 13:26:29 +0530

    wine-registration-page

--- application/controllers/Virtual.php
+++ application/controllers/Virtual.php
@@ -5,12 +5,12 @@ class Virtual extends CI_Controller {
 
 	public function index(){
 		$data['meta_title']			= 'Virtual Wine Tasting Event Registration | Odessa';
-		$data['meta_description']	= 'Join Odessa for a virtual wine tasting event. An exclusive, limited invitation event for asset finance and equipment leasing executives on May 20, 2024.';
+		$data['meta_description']	= 'Join our exclusive virtual wine-tasting event for asset finance professionals. Register now to secure your spot and enjoy an evening of fine wine and insightful discussions.';
 		$data['meta_keyword']		= 'Virtual wine tasting, wine event registration, asset finance networking, equipment leasing event, wine tasting with Haley Moore, Odessa events, executive wine tasting, wine tasting registration';
 		$data['og_title']			= 'Virtual Wine Tasting Event Registration | Odessa';
-		$data['og_description']		= 'Join Odessa for a virtual wine tasting event. An exclusive, limited invitation event for asset finance and equipment leasing executives on May 20, 2024.';
+		$data['og_description']		= 'Join our exclusive virtual wine-tasting event for asset finance professionals. Register now to secure your spot and enjoy an evening of fine wine and insightful discussions.';
 		$data['tc_title']			= 'Virtual Wine Tasting Event Registration | Odessa';
-		$data['tc_description']		= 'Join Odessa for a virtual wine tasting event. An exclusive, limited invitation event for asset finance and equipment leasing executives on May 20, 2024.';
+		$data['tc_description']		= 'Join our exclusive virtual wine-tasting event for asset finance professionals. Register now to secure your spot and enjoy an evening of fine wine and insightful discussions.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
 		$this->load->view('pages/amp_pages/virtual-wine-table-registration.html', $data);
