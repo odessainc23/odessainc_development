2657f0cb54a6a67a413c2fea59473603842045b2
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-10-17 14:54:26 +0530

    xass-edit

--- application/controllers/Ready_amp.php
+++ application/controllers/Ready_amp.php
@@ -3,13 +3,13 @@ defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 
 class Ready_amp extends CI_Controller {
 public function index(){
-	$data['meta_title']			= 'Rethink Asset Finance: Master Pay-Per-Use Models | Download the Whitepaper';
-	$data['meta_description']	= 'Transform your asset finance strategy with our expert whitepaper on pay-per-use models. Learn how to stay competitive with customer-centric and subscription-based approaches in the XaaS revolution. Get insights from industry expert Yann Toutant.';
+	$data['meta_title']			= 'Master as-a-Service Offerings in Asset Finance | Transform Your Business with Subscription-Based Models';
+	$data['meta_description']	= 'Download our whitepaper to master as-a-service models in asset finance. Gain insights on customer-centric strategies, operational excellence, and expert guidance from Yann Toutant, CEO of Black Winch.';
 	$data['meta_keyword']		= 'asset finance, pay-per-use, XaaS, subscription-based models, customer-centric approach, operational excellence, whitepaper, Yann Toutant, Everything-as-a-Service';
-	$data['og_title']			= 'Rethink Asset Finance: Master Pay-Per-Use Models | Download the Whitepaper';
-	$data['og_description']		= 'Transform your asset finance strategy with our expert whitepaper on pay-per-use models. Learn how to stay competitive with customer-centric and subscription-based approaches in the XaaS revolution. Get insights from industry expert Yann Toutant.';
-	$data['tc_title']			= 'Rethink Asset Finance: Master Pay-Per-Use Models | Download the Whitepaper';
-	$data['tc_description']		= 'Transform your asset finance strategy with our expert whitepaper on pay-per-use models. Learn how to stay competitive with customer-centric and subscription-based approaches in the XaaS revolution. Get insights from industry expert Yann Toutant.';
+	$data['og_title']			= 'Master as-a-Service Offerings in Asset Finance | Transform Your Business with Subscription-Based Models';
+	$data['og_description']		= 'Download our whitepaper to master as-a-service models in asset finance. Gain insights on customer-centric strategies, operational excellence, and expert guidance from Yann Toutant, CEO of Black Winch.';
+	$data['tc_title']			= 'Master as-a-Service Offerings in Asset Finance | Transform Your Business with Subscription-Based Models';
+	$data['tc_description']		= 'Download our whitepaper to master as-a-service models in asset finance. Gain insights on customer-centric strategies, operational excellence, and expert guidance from Yann Toutant, CEO of Black Winch.';
 
 		$this->load->view('layouts/amp_pages/header.amp.html', $data);
 		$this->load->view('pages/amp_pages/ready-to-rethink.html', $data);
