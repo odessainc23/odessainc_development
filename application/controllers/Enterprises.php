bd4040c819e2de7cf39915a77ac345169bcc9e5b
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-10-24 15:50:48 +0530

    blog-add

--- application/controllers/Enterprises.php
+++ application/controllers/Enterprises.php
@@ -12,7 +12,7 @@ class Enterprises extends CI_Controller {
 		$data['tc_title']			= 'How Modern Software Saves Enterprises Money | By Eric Bernstein, CEO of Odessa';
 		$data['tc_description']		= 'Discover how modern, cloud-based software reduces costs, enhances efficiency, and future-proofs enterprises. Learn the hidden costs of legacy systems and the benefits of modernization.';
 
-		$this->load->view('layouts/amp_pages/header.amp.html', $data);
+		$this->load->view('layouts/amp_pages/header-copy.amp.html', $data);
 		$this->load->view('pages/amp_pages/how-modern-software-saves-enterprises-money.html', $data);
 		// $this->load->view('pages/amp_pages/automotive-finance-software.html', $data);
 		$this->load->view('layouts/amp_pages/footer.amp.html', $data);
