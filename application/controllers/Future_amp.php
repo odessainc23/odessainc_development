2651a4ea740357041c869c753563f76d72bf2cbd
Krathi Shetty <Krathi.Shetty@odessainc.com>
2024-11-06 14:26:31 +0530

    edits

--- application/controllers/Future_amp.php
+++ application/controllers/Future_amp.php
@@ -1,7 +1,7 @@
 <?php
 defined( 'BASEPATH' )OR exit( 'No direct script access allowed' );
 
-class Api_amp extends CI_Controller {
+class Future_amp extends CI_Controller {
 
 	public function index(){
 		$data['meta_title']			= 'Unlocking Asset Finance Futures: Mastering API Strategies for Success';
